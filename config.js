2ee4a518f9c85acb27c5ddae886a4b240c0999a1
imldy <im.ldy@outlook.com>
2023-07-19 19:16:40 +0800

    dev: 从配置中获取接口

---
+++ config.js
@@ -0,0 +1,14 @@
+const apis = [
+    {
+        name: "【官网直连】api.openai.com",
+        baseURL: "https://api.openai.com"
+    },
+    {
+        name: "【CloudFlare】cf.api.openai.imldy.link",
+        baseURL: "https://cf.api.openai.imldy.link"
+    },
+    {
+        name: "【Vercel】api.openai.imldy.link",
+        baseURL: "https://api.openai.imldy.link"
+    },
+]
